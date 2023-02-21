2266df9ace5aa618613f2f0d84ff9808e801e88b
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-02-21 18:12:38 +0100

    Js change

--- app.js
+++ app.js
@@ -4,23 +4,43 @@ window.addEventListener("load", start);
 
 function start() {
   console.log("start");
-  document.querySelector("#civil_container").addEventListener("click", zoom);
-  document.querySelector("#bandit1_container").addEventListener("click", zoom2);
+  document
+    .querySelector("#civil_container")
+    .addEventListener("click", zoom_Civil1);
+  document
+    .querySelector("#bandit1_container")
+    .addEventListener("click", zoom_Bandit1);
 }
 
-function zoom() {
+function zoom_Civil1() {
+  document.querySelector("#civil_container").removeEventListener("click", start);
+
   document.querySelector("#civil_container").classList.add("paused");
+
   document.querySelector("#civil_sprite").classList.add("zoom_out");
-  
+
+  document.querySelector("#civil_container").addEventListener("animationend", repeatC1)
+
 }
-document.querySelector("#civil_container").removeEventListener("click", start);
 
+function repeatC1() {
+  document.querySelector("#civil_container").removeEventListener("animationend", repeatC1)
 
-function zoom2() {
+  document.querySelector("#civil_sprite").classList.remove("zoom_out");
+
+  document.querySelector("#civil_container").classList.remove("paused");
+
+  document.querySelector("#civil_container").classList.remove("civil1");
+  document.querySelector("#civil_container").offsetWidth;
+  document.querySelector("#civil_container").classList.add("civil1");
+
+  document.querySelector("#civil_container").addEventListener("click", zoom_Civil1);
+}
+
+function zoom_Bandit1() {
   document.querySelector("#bandit1_container").classList.add("paused");
   document.querySelector("#bandit1_sprite").classList.add("zoom_out");
+  document
+    .querySelector("#bandit1_container")
+    .removeEventListener("click", start);
 }
-
-document.querySelector("bandit1_container").removeEventListener("click", start);
-
-
