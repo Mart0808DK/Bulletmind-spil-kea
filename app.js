c3b5a9a64f3a1307424b030780a15fc5c7be4618
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-02-21 15:51:11 +0100

    JavaScript spil

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+"use script";
+
 window.addEventListener("load", start);
 
 function start() {
   console.log("start");
   document.querySelector("#civil_container").addEventListener("click", zoom);
+  document.querySelector("#bandit1_container").addEventListener("click", zoom2);
 }
 
 function zoom() {
   document.querySelector("#civil_container").classList.add("paused");
   document.querySelector("#civil_sprite").classList.add("zoom_out");
+  
 }
 document.querySelector("#civil_container").removeEventListener("click", start);
+
+
+function zoom2() {
+  document.querySelector("#bandit1_container").classList.add("paused");
+  document.querySelector("#bandit1_sprite").classList.add("zoom_out");
+}
+
+document.querySelector("bandit1_container").removeEventListener("click", start);
 
 
