68cab70ac90a383b63008a0ca70130d0c94c4f79
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-02-21 18:29:50 +0100

    bandit1 animation

--- app.js
+++ app.js
@@ -13,18 +13,23 @@ function start() {
 }
 
 function zoom_Civil1() {
-  document.querySelector("#civil_container").removeEventListener("click", start);
+  document
+    .querySelector("#civil_container")
+    .removeEventListener("click", start);
 
   document.querySelector("#civil_container").classList.add("paused");
 
   document.querySelector("#civil_sprite").classList.add("zoom_out");
 
-  document.querySelector("#civil_container").addEventListener("animationend", repeatC1)
-
+  document
+    .querySelector("#civil_container")
+    .addEventListener("animationend", repeatC1);
 }
 
 function repeatC1() {
-  document.querySelector("#civil_container").removeEventListener("animationend", repeatC1)
+  document
+    .querySelector("#civil_container")
+    .removeEventListener("animationend", repeatC1);
 
   document.querySelector("#civil_sprite").classList.remove("zoom_out");
 
@@ -34,13 +39,39 @@ function repeatC1() {
   document.querySelector("#civil_container").offsetWidth;
   document.querySelector("#civil_container").classList.add("civil1");
 
-  document.querySelector("#civil_container").addEventListener("click", zoom_Civil1);
+  document
+    .querySelector("#civil_container")
+    .addEventListener("click", zoom_Civil1);
 }
 
 function zoom_Bandit1() {
-  document.querySelector("#bandit1_container").classList.add("paused");
-  document.querySelector("#bandit1_sprite").classList.add("zoom_out");
   document
     .querySelector("#bandit1_container")
     .removeEventListener("click", start);
+
+  document.querySelector("#bandit1_container").classList.add("paused");
+
+  document.querySelector("#bandit1_sprite").classList.add("zoom_out");
+
+  document
+    .querySelector("#bandit1_container")
+    .addEventListener("animationend", repeatB1);
+}
+
+function repeatB1() {
+  document
+    .querySelector("#bandit1_container")
+    .removeEventListener("animationend", repeatB1);
+
+  document.querySelector("#bandit1_sprite").classList.remove("zoom_out");
+
+  document.querySelector("#bandit1_container").classList.remove("paused");
+
+  document.querySelector("#bandit1_container").classList.remove("band1");
+  document.querySelector("#bandit1_container").offsetWidth;
+  document.querySelector("#bandit1_container").classList.add("band1");
+
+  document
+    .querySelector("#bandit1_container")
+    .addEventListener("click", zoom_Bandit1);
 }
