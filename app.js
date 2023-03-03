c2082a21269a22c76427b2355f0af451af2cb248
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-03-03 12:19:43 +0100

    Omstrukturering
    time bar tilføjet og omstrukturering af start-game_ over og level complete

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 
 let points = 0;
 let lives = 3;
-const startingMinutes = 2;
-let time = startingMinutes * 60;
+
 
 const countdownEl = document.getElementById("countdown");
 
@@ -28,6 +27,7 @@ function startGame() {
   resetLives();
   resetPoint();
   showGameScreen();
+  time_bar();
   document.querySelector("#civil_container").classList.add("civil1");
   document.querySelector("#civil_container2").classList.add("civil2");
   document.querySelector("#civil_container3").classList.add("civil3");
@@ -39,33 +39,34 @@ function startGame() {
   document.querySelector("#sound_start_game").loop = true;
   document.querySelector("#sound_start_game").volume = 0.2;
   document.querySelector("#start_g").classList.add("hidden");
-
+ 
   document
     .querySelector("#civil_container")
-    .addEventListener("click", zoom_Civil1);
+    .addEventListener("mousedown", zoom_Civil1);
   document
     .querySelector("#civil_container2")
-    .addEventListener("click", zoom_Civil2);
+    .addEventListener("mousedown", zoom_Civil2);
   document
     .querySelector("#civil_container3")
-    .addEventListener("click", zoom_Civil3);
+    .addEventListener("mousedown", zoom_Civil3);
   document
     .querySelector("#bandit1_container")
-    .addEventListener("click", zoom_Bandit1);
+    .addEventListener("mousedown", zoom_Bandit1);
   document
     .querySelector("#bandit2_container")
-    .addEventListener("click", zoom_Bandit2);
+    .addEventListener("mousedown", zoom_Bandit2);
   document
     .querySelector("#bandit3_container")
-    .addEventListener("click", zoom_Bandit3);
+    .addEventListener("mousedown", zoom_Bandit3);
 }
 
 function zoom_Civil1() {
   console.log("civil 1 hit");
+  document.querySelector("#sound_civil").currentTime = 0;
   document.querySelector("#sound_civil").play();
   let civ1 = document.querySelector("#civil_container");
 
-  civ1.removeEventListener("click", zoom_Civil1);
+  civ1.removeEventListener("mousedown", zoom_Civil1);
 
   civ1.classList.add("paused");
 
@@ -90,15 +91,16 @@ function repeatC1() {
   restartC1.offsetWidth;
   restartC1.classList.add("civil1");
 
-  restartC1.addEventListener("click", zoom_Civil1);
+  restartC1.addEventListener("mousedown", zoom_Civil1);
 }
 
 function zoom_Civil2() {
   console.log("civil 2 hit");
+  document.querySelector("#sound_civil").currentTime = 0;
   document.querySelector("#sound_civil").play();
   let civ2 = document.querySelector("#civil_container2");
 
-  civ2.removeEventListener("click", zoom_Civil2);
+  civ2.removeEventListener("mousedown", zoom_Civil2);
 
   civ2.classList.add("paused");
 
@@ -123,14 +125,16 @@ function repeatC2() {
   restartC2.offsetWidth;
   restartC2.classList.add("civil2");
 
-  restartC2.addEventListener("click", zoom_Civil2);
+  restartC2.addEventListener("mousedown", zoom_Civil2);
 }
 
 function zoom_Civil3() {
   console.log("civil 3 hit");
+  document.querySelector("#sound_civil").currentTime = 0;
   document.querySelector("#sound_civil").play();
   let civ3 = document.querySelector("#civil_container3");
-  civ3.removeEventListener("click", zoom_Civil3);
+
+  civ3.removeEventListener("mousedown", zoom_Civil3);
 
   civ3.classList.add("paused");
 
@@ -155,7 +159,7 @@ function repeatC3() {
   restartC3.offsetWidth;
   restartC3.classList.add("civil3");
 
-  restartC3.addEventListener("click", zoom_Civil3);
+  restartC3.addEventListener("mousedown", zoom_Civil3);
 }
 
 function zoom_Bandit1() {
@@ -164,7 +168,7 @@ function zoom_Bandit1() {
   document.querySelector("#sound_bandit_gun").play();
   let ban1 = document.querySelector("#bandit1_container");
 
-  ban1.removeEventListener("click", zoom_Bandit1);
+  ban1.removeEventListener("mousedown", zoom_Bandit1);
 
   ban1.classList.add("paused");
 
@@ -189,7 +193,7 @@ function repeatB1() {
   restartB1.offsetWidth;
   restartB1.classList.add("band1");
 
-  restartB1.addEventListener("click", zoom_Bandit1);
+  restartB1.addEventListener("mousedown", zoom_Bandit1);
 }
 
 function zoom_Bandit2() {
@@ -198,7 +202,7 @@ function zoom_Bandit2() {
   document.querySelector("#sound_bandit_gun").play();
   let ban2 = document.querySelector("#bandit2_container");
 
-  ban2.removeEventListener("click", zoom_Bandit2);
+  ban2.removeEventListener("mousedown", zoom_Bandit2);
 
   ban2.classList.add("paused");
 
@@ -223,7 +227,7 @@ function repeatB2() {
   restartB2.offsetWidth;
   restartB2.classList.add("band2");
 
-  restartB2.addEventListener("click", zoom_Bandit2);
+  restartB2.addEventListener("mousedown", zoom_Bandit2);
 }
 
 function zoom_Bandit3() {
@@ -232,7 +236,7 @@ function zoom_Bandit3() {
   document.querySelector("#sound_bandit_gun").play();
   let ban3 = document.querySelector("#bandit3_container");
 
-  ban3.removeEventListener("click", zoom_Bandit3);
+  ban3.removeEventListener("mousedown", zoom_Bandit3);
 
   ban3.classList.add("paused");
 
@@ -257,7 +261,7 @@ function repeatB3() {
   restartB3.offsetWidth;
   restartB3.classList.add("band2");
 
-  restartB3.addEventListener("click", zoom_Bandit3);
+  restartB3.addEventListener("mousedown", zoom_Bandit3);
 }
 
 function incrementPoints() {
@@ -294,6 +298,7 @@ function level_Complete1() {
   document.querySelector("#level_complete").classList.remove("hidden");
   document.querySelector("#sound_start_game").pause();
   document.querySelector("#sound_level_complete").play();
+  resetTimer();
 }
 
 function game_Over1() {
@@ -302,6 +307,7 @@ function game_Over1() {
   document.querySelector("#game_over").classList.remove("hidden");
   document.querySelector("#sound_start_game").pause();
   document.querySelector("#sound_game_over").play();
+  resetTimer();
 }
 
 function game_start() {
@@ -309,22 +315,12 @@ function game_start() {
   document.querySelector("#start_g").classList.remove("hidden");
 }
 
-setInterval(updateCountdown, 1000);
-
-function updateCountdown() {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-
-  countdownEl.innerHTML = `${minutes}: ${seconds}`;
-  time--;
-}
 
 function showStartScreen() {
   document.querySelector("#start_g").classList.remove("hidden");
   document.querySelector("#game_over").classList.add("hidden");
   document.querySelector("#level_complete").classList.add("hidden");
+  resetTimer();
 }
 
 function resetLives() {
@@ -365,4 +361,26 @@ function stopAnimation() {
   document.querySelector("#bandit2_container").offsetWidth;
   document.querySelector("#bandit3_container").classList.remove("band3");
   document.querySelector("#bandit3_container").offsetWidth;
+}
+
+function time_bar() {
+  document.querySelector("#time_bar_sprite").classList.add("shrink");
+  document.querySelector("#time_bar_sprite").addEventListener("animationend", startGame);
+}
+
+function timeisUp() {
+  restart_time_bar();
+  if (points == 50) {
+    level_Complete1();
+  } else {
+    game_Over1()
+  }
+}
+
+function resetTimer() {
+  document.querySelector("#time_bar_sprite").removeEventListener("animationend", timeisUp);
+  document.querySelector("#time_bar_sprite").classList.remove("shrink");
+  document.querySelector("#time_bar_sprite").offsetWidth;
+  document.querySelector("#time_bar_sprite").classList.add("shrink");
+  document.querySelector("#time_bar_sprite").addEventListener("#animationend", timeisUp);
 }
